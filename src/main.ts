893bb5b27b1ff95716b41721a4897d5503fc0215
yosmisyael <yosmisyael7308@gmail.com>
2025-03-07 20:35:53 +0700

    feat: registered global logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { INestApplicationContext } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const logger: INestApplicationContext = app.get(WINSTON_MODULE_NEST_PROVIDER);
+  app.use(logger);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
