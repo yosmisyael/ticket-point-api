24ee9829891c470f58065a25c80db9efd7a55f2d
yosmisyael <yosmisyael7308@gmail.com>
2025-03-07 20:34:31 +0700

    feat: created error filter

---
+++ src/common/error.filter.ts
@@ -0,0 +1,33 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { ZodError } from 'zod';
+import { Response } from 'express';
+
+@Catch(ZodError, HttpException)
+export class ErrorFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost): any {
+    const response = host.switchToHttp().getResponse<Response>();
+
+    if (exception instanceof HttpException) {
+      response.status(exception.getStatus()).json({
+        error: exception.getResponse(),
+      });
+    } else if (exception instanceof ZodError) {
+      response.status(400).json({
+        error: exception.message,
+      });
+    } else if (exception instanceof Error) {
+      response.status(500).json({
+        error: exception.message,
+      });
+    } else {
+      response.status(500).json({
+        error: 'An unknown error occurred.',
+      });
+    }
+  }
+}
