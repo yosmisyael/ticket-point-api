cc3843e65daf344637d4ba64fc61af24bbca44df
yosmisyael <yosmisyael7308@gmail.com>
2025-03-08 15:46:08 +0700

    feat: created custom decorator and guards for auth

---
+++ src/auth/decorators/public.decorator.ts
@@ -0,0 +1,14 @@
+import { SetMetadata } from '@nestjs/common';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
+// src/modules/auth/decorators/current-user.decorator.ts
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const CurrentUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+  },
+);
