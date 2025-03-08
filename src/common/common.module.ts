0fc842f9fcc69846028857ac8d234e78209d0f77
yosmisyael <yosmisyael7308@gmail.com>
2025-03-08 14:09:28 +0700

    feat: added error filters as provider

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -4,6 +4,8 @@ import { ValidationService } from './validation.service';
 import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
+import { ErrorFilter } from './error.filter';
 
 @Global()
 @Module({
@@ -15,6 +17,14 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+  ],
+  providers: [
+    PrismaService,
+    ValidationService,
+    {
+      provide: APP_FILTER,
+      useClass: ErrorFilter,
+    }
   ],
   exports: [PrismaService, ValidationService],
 })
