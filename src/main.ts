39bffa60f00680a07012c7d8baf88a2002aef882
yosmisyael <yosmisyael7308@gmail.com>
2025-03-08 13:52:00 +0700

    refactor: removed unused import

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
-import { INestApplicationContext } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
