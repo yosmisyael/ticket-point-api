b908324e9f9cdb18052816b9a1e8d41b6d05eb7b
yosmisyael <yosmisyael7308@gmail.com>
2025-03-09 19:11:49 +0700

    feat: changed auth service logic to return jwt token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,14 +2,16 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthDto, AuthRequestDto } from './dto/auth.dto';
 import { PrismaService } from '../common/prisma.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly prismaService: PrismaService,
+    private readonly jwtService: JwtService
   ) {}
 
-  async validateUser({ email, password }: AuthRequestDto): Promise<AuthDto> {
+  async validateUser({ email, password }: AuthRequestDto): Promise<string> {
     const user = await this.prismaService.user.findUnique({
       where: { email },
     })
@@ -24,9 +26,14 @@ export class AuthService {
       throw new UnauthorizedException('Email or password is wrong');
     }
 
-    return {
-      email: user.email,
-      name: user.name,
-    }
+    const jwtPayload: AuthDto = { email: user.email, name: user.name };
+
+    const token = this.jwtService.sign(jwtPayload);
+
+    await this.prismaService.authentication.create({
+      data: { token }
+    })
+
+    return token;
   }
 }
