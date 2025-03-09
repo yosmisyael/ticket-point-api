2ee600b510ce590383fe9d8014ce245c1213d697
yosmisyael <yosmisyael7308@gmail.com>
2025-03-09 19:58:02 +0700

    fix: fixed errors missing injectable decorator

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,13 +1,17 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport';
+import { Strategy } from 'passport-local';
 import { AuthService } from '../auth.service';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-    super();
+    console.log('inside constructor local strategy')
+    super({ usernameField: 'email' });
   }
 
   async validate(email: string, password: string): Promise<string> {
+    console.log('inside validate local strategy');
     return await this.authService.validateUser({ email, password });
   }
 }
