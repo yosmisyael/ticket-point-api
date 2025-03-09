a088b545d8a55b0664c45ee6e9a48a031b751fa5
yosmisyael <yosmisyael7308@gmail.com>
2025-03-09 19:20:00 +0700

    feat: created auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+
+@Module({
+  imports: [
+    PassportModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('ACCESS_TOKEN_KEY'),
+        signOptions: {
+          expiresIn: configService.get<string>('ACCESS_TOKEN_AGE', '1d')
+        }
+      })
+    })
+  ]
+})
+export class AuthModule {}
