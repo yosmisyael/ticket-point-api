e9d9a1b486542b52be93e3620d7d889f5192c367
yosmisyael <yosmisyael7308@gmail.com>
2025-03-09 20:18:24 +0700

    feat: imported auth module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,8 +3,10 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { MailService } from '../mail/mail.service';
 import { VerificationService } from '../verification/verification.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   providers: [UserService, MailService, VerificationService],
   controllers: [UserController]
 })
