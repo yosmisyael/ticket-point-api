1430029bb1ff27eb1073445cd671931c36e69451
yosmisyael <yosmisyael7308@gmail.com>
2025-03-10 21:06:39 +0700

    feat: imported tier module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { CommonModule } from './common/common.module';
 import { UserModule } from './user/user.module';
 import { MailModule } from './mail/mail.module';
 import { VerificationModule } from './verification/verification.module';
+import { EventModule } from './event/event.module';
+import { TierModule } from './tier/tier.module';
 
 @Module({
-  imports: [CommonModule, UserModule, MailModule, VerificationModule],
+  imports: [CommonModule, UserModule, MailModule, VerificationModule, EventModule, TierModule],
   controllers: [AppController],
   providers: [AppService],
 })
