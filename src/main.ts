ef8d909cf458bbaa53fdfcda4c88fa4c88e80f17
yosmisyael <yosmisyael7308@gmail.com>
2025-03-13 19:32:31 +0700

    feat: enabled cors

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
 
   app.useLogger(logger);
 
+  app.enableCors();
+
   app.useStaticAssets(join(__dirname, '..', 'public'));
 
   await app.listen(process.env.PORT ?? 3000);
